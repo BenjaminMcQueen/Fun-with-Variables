f3d11ff8ceb4fe0837f4dcee834400de49e48f87
BenaSPACE <BenaSPACE@gmail.com>
2023-07-19 19:22:29 -0400

    Job done

---
+++ app.js
@@ -0,0 +1,14 @@
+var a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10;
+
+console.log(a+b);
+console.log(c+d);
+console.log(e-f);
+console.log(h-g);
+console.log(i*j);
+console.log(a*c);
+console.log(d/b);
+console.log(e/c);
+f++;
+g++;
+h--;
+i--;
